bddec8b1eae7606c13456a36fc6bdb63a1c6fad2
voyager <voyager2511@outlook.com>
2016-07-10 21:01:25 +0800

    增加fragment以及分支提交代码测试

---
+++ app/src/main/java/com/runbuddy/runbuddy/fragment/RunFragment.java
@@ -0,0 +1,14 @@
+package com.runbuddy.runbuddy.fragment;
+
+import android.app.Fragment;
+import android.support.v4.view.ViewPager;
+
+/**
+ * Created by Jonney Chou on 2016/7/10.
+ */
+public class RunFragment extends Fragment {
+    private ViewPager mViewPage;
+
+
+
+}
